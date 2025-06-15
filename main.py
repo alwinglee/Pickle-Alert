b669a87c9cf1965e20f48278a73afb539e43af5f
alwinglee <alwin.g.lee@gmail.com>
2025-06-15 18:49:55 -0400

    Move the report generation logic to its own module

--- main.py
+++ main.py
@@ -1,34 +1,13 @@
-from dotenv import load_dotenv
+
 from weatherapi import WeatherAPI
 from location import Location
 from daylight import Daylight
 from rain import Rain
 from wind import Wind
+from report import Report
 
 # Number of days to forecast
 FORECAST_DAYS = 3
-
-# TODO: Update DocString
-def formatReport(locationDetails,daylightDetails,rainDetails,windDetails):
-    """
-    Takes the string representation of each class and formats it into a customized report.
-
-    :param locationDetails: Customized string from the Location class containing information about the location.
-    :param daylightDetails: Customized string from the Daylight class containing information about sunrise and sunset.
-
-    :return: None
-    """
-    print(f"{locationDetails}\n"
-          f"{daylightDetails}\n"
-          f"\n- - - SUMMARY - - -\n"
-          f"RAIN:\n"
-          f"- {rainDetails.rainSummary()}\n"
-          f"WIND SPEED:\n"
-          f"- {windDetails.windSpeedSummary()}\n"
-          f"WIND GUST:\n"
-          f"{windDetails}")
-
-
 
 
 def main():
@@ -42,8 +21,7 @@ def main():
     daylightDetails = Daylight(weatherData)
     rainDetails = Rain(weatherData)
     windDetails = Wind(weatherData)
-    formatReport(locationDetails,daylightDetails,rainDetails,windDetails)
-
+    Report.formatReport(locationDetails,daylightDetails,rainDetails,windDetails)
 main()
 
 
