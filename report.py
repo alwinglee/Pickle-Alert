7e7d7dec0391a99e3b4968f7f4eb1a372d461aa5
alwinglee <alwin.g.lee@gmail.com>
2025-06-18 20:20:40 -0400

    - Add get_am_or_pm function to append AM/PM suffix to time range. - Refactor variable names to follow snake_case naming convention.

--- report.py
+++ report.py
@@ -2,25 +2,44 @@ class Report:
     """"
     A class that formats custom string builders from all classes and generates a structured report.
     """
-    def formatReport(locationDetails, daylightDetails, rainDetails, windDetails):
+    def get_am_or_pm(time_range_hour):
+        """
+        Assigns an 'AM' or 'PM' suffix to the given time passed into the function
+
+        :param time_range_hour: An integer representing either the start or end hour of the time range.
+
+        :return:  the time including the appropriate 'AM' or 'PM' designation.
+        """
+        if (time_range_hour <12):
+            return f"{time_range_hour} AM"
+        else:
+            return f"{time_range_hour} PM"
+
+    def format_report(location_details, daylight_details, rain_details, wind_details,START_TIME, END_TIME):
         """
         Takes the string representation of each class and formats it into a customized report.
 
-        :param locationDetails: A custom string from the Location class containing location information.
-        :param daylightDetails: A custom string from the Daylight class containing sunrise and sunset times.
-        :param rainDetails: A custom string from the Rain class indicating the times of day it will rain and the rainfall
+        :param location_details: A custom string from the Location class containing location information.
+        :param daylight_details: A custom string from the Daylight class containing sunrise and sunset times.
+        :param rain_details: A custom string from the Rain class indicating the times of day it will rain and the rainfall
          amount in millimeters.
-        :param windDetails: A custom string from the Wind class containing the hours of the highest wind speed and
+        :param wind_details: A custom string from the Wind class containing the hours of the highest wind speed and
+        wind gust.
+        :param START_TIME: A custom string from the Wind class containing the hours of the highest wind speed and
+        wind gust.
+        :param END_TIME: A custom string from the Wind class containing the hours of the highest wind speed and
         wind gust.
 
         :return: None
         """
-        print(f"{locationDetails}\n"
-              f"{daylightDetails}\n"
+        print(f"{location_details}\n"
+              f"{daylight_details}\n"
               f"\n- - - SUMMARY - - -\n"
+              f"ANALYSIS PERIOD: {Report.get_am_or_pm(START_TIME)} - {Report.get_am_or_pm(END_TIME)}\n"
               f"RAIN:\n"
-              f"- {rainDetails.rainSummary()}\n"
+              f"- {rain_details.rainSummary()}\n"
               f"WIND SPEED:\n"
-              f"- {windDetails.windSpeedSummary()}\n"
+              f"- {wind_details.windSpeedSummary()}\n"
               f"WIND GUST:\n"
-              f"{windDetails}")
+              f"- {wind_details.windGustSummary()}\n"
+              f"{wind_details}")
