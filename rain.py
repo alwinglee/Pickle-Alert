97aa1ab3598e0749141e1b29a01202e0ebf551fd
alwinglee <alwin.g.lee@gmail.com>
2025-06-18 20:33:58 -0400

    Refactor variable names to follow snake_case naming convention.

--- rain.py
+++ rain.py
@@ -4,67 +4,67 @@ class Rain:
     """
     A class to filter, analyze, and report rain-related weather data.
     """
-    def __init__(self,weatherData):
+    def __init__(self,weather_data):
         """
         Initializes the Rain class with key rain-related metrics from an API response and sets evaluation thresholds
         for rain impact levels.
 
-        :param weatherData: Weather forecast response in JSON format.
+        :param weather_data: Weather forecast response in JSON format.
 
         :return: None
         """
-        self.weatherData=weatherData
-        self.stringBuilder = StringIO()
-        self.willItRain = self.weatherData["forecast"]["forecastday"][0]["day"]["daily_will_it_rain"]
-        self.chanceOfRain = self.weatherData["forecast"]["forecastday"][0]["day"]["daily_chance_of_rain"]
-        self.totalPrecipitation = self.weatherData["forecast"]["forecastday"][0]["day"]["totalprecip_mm"]
-        self.rainDescription = self.weatherData["forecast"]["forecastday"][0]["day"]["condition"]["text"]
+        self.weather_data=weather_data
+        self.string_builder = StringIO()
+        self.will_it_rain = self.weather_data["forecast"]["forecastday"][0]["day"]["daily_will_it_rain"]
+        self.chance_of_rain = self.weather_data["forecast"]["forecastday"][0]["day"]["daily_chance_of_rain"]
+        self.total_precipitation = self.weather_data["forecast"]["forecastday"][0]["day"]["totalprecip_mm"]
+        self.rain_description = self.weather_data["forecast"]["forecastday"][0]["day"]["condition"]["text"]
         self.RAIN_CHANCE_LOW_THRESH=30
         self.RAIN_CHANCE_HIGH_THRESH = 70
         self.RAIN_PRECIPITATION_LOW_THRESH=2.5
         self.RAIN_PRECIPITATION_HIGH_THRESH=7.5
 
-    def rainSummary(self):
+    def rain_summary(self):
         """
         Generates a summary of the rain condition for the day, including impact assessments.
 
         :return: String containing the day's rain status, including impact level description when applicable.
         """
-        if (self.willItRain ==0):
-            return ("Will It Rain: NO")
+        if (self.will_it_rain ==0):
+            return (f"Will It Rain: NO")
         else:
-            self.rainTimeline()
-            return(f"Will It Rain: YES ({self.rainDescription})\n"
-                  f"Chance of Rain:{self.chanceOfRainImpact()}IMPACT ({self.chanceOfRain}%) \n"
-                  f"Total Precipitation: {self.totalPrecipitationImpact()} IMPACT ({self.totalPrecipitation}mm)")
+            self.rain_timeline()
+            return(f"Will It Rain: YES ({self.rain_description})\n"
+                  f"Chance of Rain:{self.chance_of_rain_impact()}IMPACT ({self.chance_of_rain}%) \n"
+                  f"Total Precipitation: {self.total_precipitation_impact()} IMPACT ({self.total_precipitation}mm)")
 
-    def chanceOfRainImpact(self):
+    def chance_of_rain_impact(self):
         """
         Classifies rain probability into one of three impact levels.
 
         :return: String containing the impact level and description.
         """
-        if (self.chanceOfRain <self.RAIN_CHANCE_LOW_THRESH):
+        if (self.chance_of_rain <self.RAIN_CHANCE_LOW_THRESH):
             return ("MINIMAL (BRIEF)")
-        elif (self.chanceOfRain <self.RAIN_CHANCE_HIGH_THRESH ):
+        elif (self.chance_of_rain <self.RAIN_CHANCE_HIGH_THRESH ):
             return ("MODERATE (SCATTERED)")
         else:
             return ("MAXIMUM (PERSISTENT)")
 
-    def totalPrecipitationImpact(self):
+    def total_precipitation_impact(self):
         """
         Determines rain precipitation into one of three impact levels.
 
         :return: String containing the impact level and description.
         """
-        if (self.totalPrecipitation<self.RAIN_PRECIPITATION_LOW_THRESH):
+        if (self.total_precipitation<self.RAIN_PRECIPITATION_LOW_THRESH):
             return ("MINIMAL (NO ACCUMULATION)")
-        elif (self.totalPrecipitation<self.RAIN_PRECIPITATION_HIGH_THRESH):
+        elif (self.total_precipitation<self.RAIN_PRECIPITATION_HIGH_THRESH):
             return ("MODERATE (PUDDLES)")
         else:
             return ("MAXIMUM (FLOODING)")
 
-    def rainTimeline(self):
+    def rain_timeline(self):
         """
         Generates hourly rain forecast data formatted as a chronological timeline.
 
@@ -75,16 +75,16 @@ class Rain:
 
         return: None
         """
-        self.stringBuilder.write("- - - RAIN REPORT - - -")
-        currentDay = self.weatherData["forecast"]["forecastday"][0]["hour"]
+        self.string_builder.write("- - - RAIN REPORT - - -")
+        currentDay = self.weather_data["forecast"]["forecastday"][0]["hour"]
         times = [
-            {   "time":datetime.strptime(each_hour["time"],("%Y-%m-%d %H:%M")).strftime("%H:%M"),"chanceOfRain":each_hour["chance_of_rain"],
+            {   "time":datetime.strptime(each_hour["time"],("%Y-%m-%d %H:%M")).strftime("%H:%M"),"chance_of_rain":each_hour["chance_of_rain"],
                 "precipitation": each_hour["precip_mm"] }for each_hour in currentDay if each_hour["will_it_rain"] !=0
             ]
-        sortByChanceOfRain = sorted(times,key=lambda item:item["chanceOfRain"],reverse=True)
-        sortByTimes=sorted(sortByChanceOfRain)
-        for each_sortedTimes in sortByTimes:
-            self.stringBuilder.write(f"{each_sortedTimes["time"]}: {each_sortedTimes["chanceOfRain"]}({each_sortedTimes["precipitation"]})")
+        sortByChanceOfRain = sorted(times,key=lambda item:item["chance_of_rain"],reverse=True)
+
+        for each_sortedTimes in sortByChanceOfRain:
+            self.string_builder.write(f"{each_sortedTimes["time"]}: {each_sortedTimes["chance_of_rain"]}({each_sortedTimes["precipitation"]})")
 
     def __str__(self):
         """
@@ -92,7 +92,7 @@ class Rain:
 
         :return: A formatted string displaying rain impact levels and forecasted rain times on separate lines.
         """
-        return self.stringBuilder.getvalue()
+        return self.string_builder.getvalue()
 
 
 
