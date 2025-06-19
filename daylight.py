25708c6a7f66a3726e68857d9f6d59dd36765daf
alwinglee <alwin.g.lee@gmail.com>
2025-06-18 20:25:42 -0400

    Refactor variable names to follow snake_case naming convention.

--- daylight.py
+++ daylight.py
@@ -2,16 +2,16 @@ class Daylight:
     """
     Represents sunrise and sunset times for a location
     """
-    def __init__(self,weatherData):
+    def __init__(self,weather_data):
         """
         Initializes Daylight with sunrise/sunset times extracted from weather API response.
 
-        :param weatherData: Weather forecast response in JSON format.
+        :param weather_data: Weather forecast response in JSON format.
 
         :return: None
         """
-        self.sunrise = weatherData["forecast"]["forecastday"][0]["astro"]["sunrise"]
-        self.sunset = weatherData["forecast"]["forecastday"][0]["astro"]["sunset"]
+        self.sunrise = weather_data["forecast"]["forecastday"][0]["astro"]["sunrise"]
+        self.sunset = weather_data["forecast"]["forecastday"][0]["astro"]["sunset"]
 
     def __str__(self):
         """
