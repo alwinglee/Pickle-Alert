71b59b30b611e8695120788cb7f53620150aa347
alwinglee <alwin.g.lee@gmail.com>
2025-06-23 17:37:00 -0400

    Add try/except block for API response handling

--- weatherapi.py
+++ weatherapi.py
@@ -6,7 +6,6 @@ class WeatherAPI:
     """
     A class to interact with a weather API and fetch forecast data.
     """
-    # TODO: Create Try Except
     def fetch_weather_forecast(FORECAST_DAYS):
         """
         Sends a request to the API server with specific parameters to retrieve the weather forecast for the current day
@@ -16,7 +15,21 @@ class WeatherAPI:
 
         :return: The API's weather forecast response in JSON format.
         """
-        load_dotenv()
-        weather_response = requests.get(os.getenv("URL"), params={"q": f"{os.getenv("LAT")},{os.getenv("LON")}",
-                                                                 "key": os.getenv("API_KEY"), "days": FORECAST_DAYS})
-        return weather_response.json()
+        try:
+            load_dotenv()
+            weather_response = requests.get(os.getenv("URL"), params={"q": f"{os.getenv("LAT")},{os.getenv("LON")}",
+                                                                      "key": os.getenv("API_KEY"), "days": FORECAST_DAYS})
+            weather_response.raise_for_status()
+            weather_response_json= weather_response.json()
+        except requests.exceptions.ConnectionError:
+            raise Exception(f"NETWORK ERROR: FAILED TO CONNECT TO SERVER")
+        except requests.exceptions.HTTPError:
+            raise Exception(f"REQUEST ERROR: SERVER REJECTED REQUEST ({weather_response.status_code} error)")
+        except requests.exceptions.JSONDecodeError:
+            raise Exception(f"JSON ERROR: INVALID JSON RETURNED")
+        except requests.exceptions.RequestException as error:
+            raise Exception(f"MISCELLANEOUS ERROR: {error}")
+        else:
+            return weather_response_json
+
+
