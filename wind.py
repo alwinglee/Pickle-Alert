2dcd29f967431b92f3f0baaebbaaa75ccbd5d09d
alwinglee <alwin.g.lee@gmail.com>
2025-06-30 13:11:16 -0400

    Refactor wind.py functions to align with temperature.py
    - Rename filter_wind_metrics() to compile_temperature_report() and refactored the function to call self.build_wind_timeline().
    - Remove the def __str__(self) function.
    - Remove the header in build_wind_timeline() and incorporate into
      compile_temperature_report().
    - wind_gust_impact() and wind_speed_impact() no longer call self.build_wind_timeline().
    - Add self.METRIC_LIST to store specific wind metric keys.

--- wind.py
+++ wind.py
@@ -9,6 +9,7 @@ class Wind:
         self.string_builder = StringIO()
         self.TOP_TIMELINE_COUNT=TOP_TIMELINE_COUNT
         self.weather_data=weather_data
+        self.METRIC_LIST=["speed","gust"]
         self.START_TIME=START_TIME
         self.END_TIME=END_TIME+1
         self.WIND_SPEED_IDEAL = 15
@@ -60,66 +61,68 @@ class Wind:
         total_sum = sum(each_hour[f"{metric}"] for each_hour in time_period_forecast)
         return round(total_sum/len(time_period_forecast))
 
-
-    def wind_summary(self):
+    def compile_wind_report(self):
         """
-        Generates a summary of daily wind conditions, including impact assessments.
+        Generates a formatted report summarizing temperature conditions
 
-        :return: String containing the day's wind conditions and impact level description (when applicable).
+        :return: A string containing the full report, structured with headers, metrics, and impact descriptions.
         """
         time_period_forecast=self.filter_wind_metrics()
-        max_wind_speed= self.find_max_wind_metric(time_period_forecast,"speed")
-        max_wind_gust=self.find_max_wind_metric(time_period_forecast,"gust")
-        average_wind_speed = self.calculate_average_wind_metric(time_period_forecast, "speed")
-        average_wind_gust = self.calculate_average_wind_metric(time_period_forecast, "gust")
-        return ("WIND SPEED:\n"
-                f"max.{max_wind_speed} kph | avg.{average_wind_speed} kph\n"
-                f"Playability: {self.wind_speed_impact(max_wind_speed, average_wind_speed)}\n"
-                f"\nWIND GUST:\n"
-                f"max.{max_wind_gust} kph | avg.{average_wind_gust} kph\n"
-                f"Impact: {self.wind_gust_impact(max_wind_gust, average_wind_gust)}\n")
+        for metric in self.METRIC_LIST:
+            display_metric_title = metric.replace("_", " ")
+            average = self.calculate_average_wind_metric(time_period_forecast,metric)
+            max = self.find_max_wind_metric(time_period_forecast,metric)
+            impact = {"speed": self.wind_speed_impact(max),
+                      "gust": self.wind_gust_impact(max)}
+            self.string_builder.write (f"\n- - - WIND {display_metric_title.upper()} REPORT - - -\n"
+                                        f"Max. {max} kph | Avg. {average} kph \n")
+            self.string_builder.write(f"Impact: {impact[metric]}\n")
+            self.build_wind_timeline(time_period_forecast,metric)
+        return self.string_builder.getvalue()
 
-    def wind_speed_impact(self,max_wind_speed, average_wind_speed):
+    def wind_speed_impact(self,max_wind_speed):
+        """
+        Generates a summary of the wind speed conditions for the day, including impact assessments.
+
+        :param max_wind_speed: An integer representing the highest wind speed in the forecast data.
+
+        :return: A string describing the day's wind speed conditions, including the impact level.
+        """
         if (max_wind_speed <= self.WIND_SPEED_IDEAL):
             return (f"IDEAL\n"
                     f"Description: BALL TRAVELS NORMALLY ")
-        elif (max_wind_speed <= self.WIND_SPEED_CHALLENGING):
-            self.build_wind_timeline("speed")
+        if (max_wind_speed <= self.WIND_SPEED_CHALLENGING):
             return (f"PLAYABLE BUT CHALLENGING\n"
                     f"Description: BALL MAY DRIFT")
         elif (max_wind_speed < self.WIND_SPEED_DIFFICULT):
-            self.build_wind_timeline("speed")
             return (f"DIFFICULT\n"
                     f"Description: STRONG BALL DRIFT")
         else:
-            self.build_wind_timeline("speed")
             return (f"IMPOSSIBLE\n"
                     f"Description: PLAY INDOORS")
 
-
-    def wind_gust_impact(self,max_wind_gust, average_wind_gust):
+    def wind_gust_impact(self,max_wind_gust):
         """
-        Generates a summary of the wind gust condition for the day, including impact assessments.
+        Generates a summary of the wind gust conditions for the day, including impact assessments.
 
-        :return: String containing the day's wind gust status, including impact level description when applicable
+        :param max_wind_gust: An integer representing the highest wind gust in the forecast data.
+
+        :return: A string describing the day's wind gust conditions, including the impact level.
         """
         if (max_wind_gust <= self.WIND_GUST_MINIMAL):
             return (f"MINIMAL\n"
                     f"Description: BALL TRAVELS NORMALLY ")
         elif (max_wind_gust <= self.WIND_GUST_MODERATE):
-            self.build_wind_timeline("gust")
             return (f"MODERATE\n"
                     f"Description: OCCASIONAL UNPREDICTABLE DRIFT")
         elif (max_wind_gust <= self.WIND_GUST_MAXIMUM):
-            self.build_wind_timeline("gust")
             return (f"MAXIMUM\n"
                     f"Description: SUDDEN UNPREDICTABLE BALL DEFLECTION")
         else:
-            self.build_wind_timeline("gust")
             return (f"BEYOND MAXIMUM\n" 
                     f"Description: PLAY INDOORS")
 
-    def build_wind_timeline(self,metric):
+    def build_wind_timeline(self,time_period_forecast,metric):
         """
         Generates hourly wind forecast data formatted as a chronological timeline.
 
@@ -129,21 +132,12 @@ class Wind:
 
         :return: None
         """
-        self.string_builder.write(f"\n- - - WIND {metric.upper()} REPORT - - -\n"
-                                 f"Top {self.TOP_TIMELINE_COUNT} Peak Wind {metric.title()} Hours:\n")
-        sort_by_max= sorted(self.filter_wind_metrics(), key=lambda item:item[f"{metric}"],reverse=True)[:self.TOP_TIMELINE_COUNT]
-        sort_by_time = sorted(sort_by_max,key=lambda item:item["time"])
+        self.string_builder.write(
+            f"- - - TOP {self.TOP_TIMELINE_COUNT} PEAK {metric.upper()} HOURS - - -\n")
+        sort_by_max = sorted(time_period_forecast, key=lambda item: item[f"{metric}"], reverse=True)[:self.TOP_TIMELINE_COUNT]
+        sort_by_time = sorted(sort_by_max, key=lambda item: item["time"])
         for each_hour in sort_by_time:
-            self.string_builder.write(f"\t{each_hour["time"]}: {each_hour[f"{metric}"]} kph\n")
-
-    def __str__(self):
-        """
-        Overrides the default '__str__' method to return wind forecast data in another format.
-
-        :return: A formatted string displaying wind impact levels and forecasted wind times on separate lines.
-        """
-        return self.string_builder.getvalue()
-
+                self.string_builder.write(f"\t{each_hour["time"]}: {each_hour[f"{metric}"]} kph\n")
 
 
 
