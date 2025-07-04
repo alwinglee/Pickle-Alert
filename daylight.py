d99852cbcec15072bb7894d3920716a8145ac191
alwinglee <alwin.g.lee@gmail.com>
2025-07-04 11:16:07 -0400

    Add a new function that converts sunrise/sunset times from standard to military time

--- daylight.py
+++ daylight.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 class Daylight:
     """
     Represents sunrise and sunset times for a location
@@ -13,12 +14,23 @@ class Daylight:
         self.sunrise = weather_data["forecast"]["forecastday"][0]["astro"]["sunrise"]
         self.sunset = weather_data["forecast"]["forecastday"][0]["astro"]["sunset"]
 
+
+    def convert_to_military_time(self,time):
+        """
+        Converts standard time from API data to military time (24-hour format) for sunrise or sunset.
+
+        :param time: Sunrise or sunset time in standard 12-hour format.
+
+        :return: Sunrise or sunset time in military (24-hour) format.
+        """
+        return datetime.strptime(time,"%I:%M %p").strftime("%H:%M")
+
     def __str__(self):
         """
         Overrides the default '__str__' method to return sunrise/sunset in another format.
 
         :return: A formatted string displaying sunrise and sunset times on separate lines.
         """
-        return (f"Sunrise: {self.sunrise}\n"
-                f"Sunset: {self.sunset}")
+        return (f"Sunrise: {self.convert_to_military_time(self.sunrise)}\n"
+                f"Sunset: {self.convert_to_military_time(self.sunset)}")
 
