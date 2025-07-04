0bec3aab61de689e26be8aa53c6400be656d5f35
alwinglee <alwin.g.lee@gmail.com>
2025-07-04 18:41:27 -0400

    Refactor class by initializing it and calling its report generation method

--- report.py
+++ report.py
@@ -1,42 +1,31 @@
+from datetime import datetime
 class Report:
-    """"
-    A class that formats custom string builders from all classes and generates a structured report.
     """
-    def get_am_or_pm(hour):
+    Gathers reports from other metric classes into the main Report class, preparing the data for full report generation
+    """
+    def __init__(self,location_details, daylight_details, rain_details, wind_details, temperature_details,START_TIME,END_TIME,total_hours):
+        self.location_details= location_details
+        self.daylight_details = daylight_details
+        self.rain_details=rain_details
+        self.wind_details = wind_details
+        self.temperature_details = temperature_details
+        self.START_TIME = datetime.strptime(f"{START_TIME:02d}", "%H").strftime("%H:%M")
+        self.END_TIME = datetime.strptime(f"{END_TIME:02d}", "%H").strftime("%H:%M")
+        self.total_hours = total_hours
+        self.format_report()
+
+    def format_report(self):
         """
-        Assigns an 'AM' or 'PM' suffix to the given time passed into the function
-
-        :param hour: An integer representing either the start or end hour of the time range.
-
-        :return:  the time including the appropriate 'AM' or 'PM' designation.
-        """
-        if (hour <12):
-            return f"{hour} AM"
-        else:
-            return f"{hour} PM"
-
-    def format_report(location_details, daylight_details, rain_details, wind_details, temperature_details, START_TIME, END_TIME):
-        """
-        Takes the string representation of each class and formats it into a customized report.
-
-        :param location_details: A custom string from the Location class containing location information.
-        :param daylight_details: A custom string from the Daylight class containing sunrise and sunset times.
-        :param rain_details: A custom string from the Rain class indicating the times of day it will rain and the rainfall
-         amount in millimeters.
-        :param wind_details: A custom string from the Wind class containing the hours of the highest wind speed and
-        wind gust.
-        :param START_TIME: A custom string from the Wind class containing the hours of the highest wind speed and
-        wind gust.
-        :param END_TIME: A custom string from the Wind class containing the hours of the highest wind speed and
-        wind gust.
+        Takes the string representation of each class and formats it into a customized report
 
         :return: None
         """
-        print(f"{location_details}\n"
-              f"{daylight_details}\n"
+        print(f"{self.location_details}\n"
+              f"{self.daylight_details}\n"
               f"\n- - - SUMMARY - - -\n"
               f"ANALYSIS PERIOD:\n"
-              f"{Report.get_am_or_pm(START_TIME)} - {Report.get_am_or_pm(END_TIME)}\n"
-              f"{rain_details.compile_wind_report()}"
-              f"{wind_details.compile_wind_report()}"
-              f"{temperature_details.compile_temperature_report()}")
+              f"{self.START_TIME} - {self.END_TIME} ({self.total_hours} hours)\n"
+              f"{self.rain_details.compile_pre_window_rain_report()}"
+              f"{self.rain_details.compile_during_window_rain_report()}"
+              f"{self.wind_details.compile_wind_report()}"
+              f"{self.temperature_details.compile_temperature_report()}")
