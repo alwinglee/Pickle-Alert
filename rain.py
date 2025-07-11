9cf507cbd8cb3a251fad7d738d51c2b099444d0f
alwinglee <alwin.g.lee@gmail.com>
2025-07-11 19:01:44 -0400

    Add a new function, assess_pre_window_impact(), to evaluate the most recent rainfall and classify its impact level

--- rain.py
+++ rain.py
@@ -20,12 +20,17 @@ class Rain:
         self.total_hours=total_hours
         self.TOP_TIMELINE_COUNT = TOP_TIMELINE_COUNT
         self.RAIN_CHECK_HOURS_PRIOR=RAIN_CHECK_HOURS_PRIOR
+        self.PRE_RAIN_WINDOW_START= self.START_TIME - self.RAIN_CHECK_HOURS_PRIOR
+        self.START_TIME_FORMATTED= datetime.strptime(f"{START_TIME:02d}:00", "%H:%M").time()
+        self.RAIN_CHECK_HOURS_PRIOR=RAIN_CHECK_HOURS_PRIOR
         self.RAIN_WEIGHTED_RAIN_PROBABILITY_LOW =30
         self.RAIN_WEIGHTED_RAIN_PROBABILITY_MODERATE = 50
         self.RAIN_PRECIPITATION_LOW=0.5
         self.RAIN_PRECIPITATION_MODERATE=2.0
         self.RAIN_COVERAGE_LOW= 30
         self.RAIN_COVERAGE_MODERATE=60
+        self.LAST_HOUR_IMPACT_LOW=3
+        self.LAST_HOUR_IMPACT_MODERATE=2
 
     def filter_rain_metric(self,START_TIME,END_TIME):
         """
@@ -112,12 +117,17 @@ class Rain:
             total_precipitation = self.calculate_total_precipitation(time_period_forecast)
             rain_coverage_percentage = self.calculate_rain_coverage_percentage(time_period_forecast)
             weighted_rain_probability = self.calculate_weighted_rain_probability(time_period_forecast)
+            weighted_rain_probability_result = self.weighted_rain_probability_impact(weighted_rain_probability)
+            rain_coverage_result=self.rain_coverage_impact(rain_coverage_percentage )
+            total_precipitation_result=self.total_precipitation_impact(total_precipitation)
             number_of_hour_of_rain = len(time_period_forecast)
 
-            string_builder.write(f"YES\n")
-            string_builder.write(
-                f"Will Rain {number_of_hour_of_rain}/{self.total_hours} hours ({rain_coverage_percentage}%) "
-                f"| Avg. Chance: {weighted_rain_probability}% | {total_precipitation} mm\n")
+            string_builder.write(f"YES\n"
+                                 f"Will Rain {number_of_hour_of_rain}/{self.total_hours} hours ({rain_coverage_percentage}%)"
+                                 f"| Avg. Chance: {weighted_rain_probability}% | {total_precipitation} mm\n")
+            string_builder.write (f"Rain Probability:{weighted_rain_probability_result}\n"
+                                 f"Rain Coverage: {rain_coverage_result}\n"
+                                 f"Precipitation: {total_precipitation_result}")
             string_builder.write(self.build_rain_timeline(time_period_forecast))
         return string_builder.getvalue()
 
@@ -128,20 +138,39 @@ class Rain:
         :return: A string containing the full report, structured with headers, metrics, and impact descriptions.
         """
         string_builder = StringIO()
-        time_period_forecast = self.filter_rain_metric((self.START_TIME - self.RAIN_CHECK_HOURS_PRIOR), self.START_TIME)
+        time_period_forecast = self.filter_rain_metric(self.PRE_RAIN_WINDOW_START, self.START_TIME)
         total_precipitation = self.calculate_total_precipitation(time_period_forecast)
-        string_builder.write(f"Precipitation last {self.RAIN_CHECK_HOURS_PRIOR} hours before {self.START_TIME}:00?: ")
+        string_builder.write(f"Did It Rain {self.RAIN_CHECK_HOURS_PRIOR} hours before {self.START_TIME}:00?: ")
         if not (time_period_forecast):
             string_builder.write(f"NO (REPORT OMITTED)\n")
         else:
-            last_hour_of_rain = (time_period_forecast[-1]["time"])
-            string_builder.write(f"YES\n")
-            string_builder.write(
-                f"Rained {len(time_period_forecast)}/{self.RAIN_CHECK_HOURS_PRIOR} hour (Last {last_hour_of_rain}) | "
-                f"{total_precipitation} mm\n")
+            last_rain_hour = (time_period_forecast[-1]["time"])
+            impact= self.assess_pre_window_impact(last_rain_hour)
+            string_builder.write(f"YES\n"
+                                 f"Rained {len(time_period_forecast)}/{self.RAIN_CHECK_HOURS_PRIOR} last hours (Last {last_rain_hour}) | "
+                                 f"{total_precipitation} mm\n")
+            string_builder.write(f"Impact: {impact}\n")
             string_builder.write(self.build_rain_timeline(time_period_forecast))
         return string_builder.getvalue()
 
+    def assess_pre_window_impact(self,last_rain_hour):
+        """
+        Assesses the last rainfall event and calculates the time difference (in hours) from the assigned start time,
+        classifying it into one of three impact levels.
+
+        :param last_rain_hour:  The last recorded hour of rainfall formatted in military time
+
+        :return: A string describing the most recent rainfall before the assigned start time, including its impact level
+        """
+        datetime_conversion = datetime.strptime(f"{last_rain_hour}", "%H:%M").time()
+        datetime_difference= datetime.combine(datetime.today(), self.START_TIME_FORMATTED) - datetime.combine(datetime.today(),datetime_conversion)
+        rain_time_difference_hours= datetime_difference.total_seconds() / 3600
+        if (rain_time_difference_hours > self.LAST_HOUR_IMPACT_LOW):
+            return ("🟩 LOW (PLAYABLE)")
+        elif (rain_time_difference_hours == self.LAST_HOUR_IMPACT_MODERATE):
+            return ("🟨 MODERATE (DELAY START/CHECK COURT CONDITIONS)")
+        else:
+            return ("🟥 HIGH (POSTPONE)")
     def weighted_rain_probability_impact(self,weighted_rain_probability):
         """
         Classifies weighted rain probability into three impact levels
@@ -150,9 +179,9 @@ class Rain:
 
         :return: A string describing the day's rain probability, including the impact level.
         """
-        if (weighted_rain_probability <self.self.RAIN_WEIGHTED_RAIN_PROBABILITY_LOW):
+        if (weighted_rain_probability <self.RAIN_WEIGHTED_RAIN_PROBABILITY_LOW):
             return ("🟩 LOW (PLAYABLE)")
-        elif (weighted_rain_probability <self.self.RAIN_WEIGHTED_RAIN_PROBABILITY_MODERATE):
+        elif (weighted_rain_probability <self.RAIN_WEIGHTED_RAIN_PROBABILITY_MODERATE):
             return ("🟨 MODERATE (CAUTION)\n")
         else:
             return ("🟥 HIGH (UNPLAYABLE)\n")
@@ -165,9 +194,9 @@ class Rain:
 
         :return: A string describing the day's rain coverage, including the impact level.
         """
-        if (rain_coverage< self.self.RAIN_COVERAGE_LOW):
+        if (rain_coverage< self.RAIN_COVERAGE_LOW):
             return ("🟩 LOW (PLAYABLE)")
-        elif (rain_coverage < self.self.RAIN_COVERAGE_MODERATE):
+        elif (rain_coverage < self.RAIN_COVERAGE_MODERATE):
             return ("🟨 MODERATE (CAUTION)\n")
         else:
             return ("🟥 HIGH (UNPLAYABLE)\n")
