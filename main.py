c8dd6dfd743cff3141cab3c61f8d742c5e217c08
alwinglee <alwin.g.lee@gmail.com>
2025-08-08 18:24:39 -0400

    Create forecast_data, forecast_location_data, and hourly_forecasts variables to reduce argument passing between classes

--- main.py
+++ main.py
@@ -12,17 +12,17 @@ from configuration import Configuration
 
 # Show Up To Which Days (E.g. 1 = current day, 2= current day + Next day, 3 = current day + next two days, and etc)
 # (Minimum: 1 day | Maximum: 14 days (currently limited to 3 days on free tier))
-DAYS_TO_SHOW = 1
+DAYS_TO_SHOW = 2
 # First hour to include in analysis (24- hour format).For single-digit hours, omit the leading zero.
 # Use 9 for 9 AM, 22 for 10 PM, and 24 for midnight (current day, not next day)
 # Example: 16-20 includes hours 16, 17, 18, and 19 (excludes 20)
-START_TIME = 16
+START_TIME = 18
 # Last hour to include in analysis (24- hour format).For single-digit hours, omit the leading zero
 # Use 9 for 9 AM, 22 for 10 PM, and 24 for midnight (current day, not next day)
 # Example: 16-20 includes hours 16, 17, 18, and 19 (excludes 20)
-END_TIME = 22
+END_TIME = 24
 # Number of hours displayed in the timeline report. Cannot exceed the time range duration specified above.
-TOP_TIMELINE_COUNT = 6
+TOP_TIMELINE_COUNT = 2
 # Specifies how many hours before the start time to check for rain (used in Rain class)
 RAIN_CHECK_HOURS_PRIOR = 4
 
@@ -35,17 +35,20 @@ def main():
     """
     try:
         Configuration(START_TIME, END_TIME, TOP_TIMELINE_COUNT, RAIN_CHECK_HOURS_PRIOR, DAYS_TO_SHOW)
-        duration = END_TIME-START_TIME
         weather_data = Weather_API.fetch_weather_forecast(DAYS_TO_SHOW)
         for forecast_day in range(DAYS_TO_SHOW):
-            location_details = Location(weather_data)
-            date_details = Date(weather_data,START_TIME,END_TIME, duration, forecast_day)
-            daylight_details = Daylight(weather_data,forecast_day)
-            condition_details = Condition(weather_data, START_TIME, END_TIME, TOP_TIMELINE_COUNT, forecast_day)
-            rain_details = Rain(weather_data, START_TIME, END_TIME, duration, TOP_TIMELINE_COUNT, RAIN_CHECK_HOURS_PRIOR, forecast_day)
-            wind_details = Wind(weather_data, START_TIME, END_TIME, TOP_TIMELINE_COUNT,forecast_day)
-            temperature_details = Temperature(weather_data, START_TIME, END_TIME, TOP_TIMELINE_COUNT,forecast_day)
-            report_details = Report(location_details, daylight_details, rain_details, wind_details, temperature_details, condition_details, date_details,  START_TIME, END_TIME, duration)
+            forecast_location_data= weather_data["location"]
+            forecast_data = weather_data["forecast"]["forecastday"][forecast_day]
+            hourly_selected_forecast_data=forecast_data["hour"][START_TIME:END_TIME]
+            location_details = Location(forecast_location_data)
+            date_details = Date(START_TIME,END_TIME,forecast_data)
+            daylight_details = Daylight(forecast_data)
+            condition_details = Condition(hourly_selected_forecast_data)
+            rain_details = Rain(START_TIME, END_TIME,TOP_TIMELINE_COUNT, RAIN_CHECK_HOURS_PRIOR, forecast_data)
+            wind_details = Wind(TOP_TIMELINE_COUNT,hourly_selected_forecast_data)
+            temperature_details = Temperature(TOP_TIMELINE_COUNT,hourly_selected_forecast_data)
+            report_details = Report(location_details, daylight_details, rain_details, wind_details, temperature_details,
+                                    condition_details, date_details, START_TIME, END_TIME)
             # Sms(report_details,date_details)
     except Exception as error:
         print(f"Report Generation Failed:\n"
