b79a8e2eac2e518a1faac9bcb1bcdf079264ac3e
alwinglee <alwin.g.lee@gmail.com>
2025-09-01 15:12:10 -0400

    Include docstring for functions

--- alert.py
+++ alert.py
@@ -1,7 +1,15 @@
 from io import StringIO
 from datetime import datetime
 class Alert:
+    """
+    Designed to manage and send weather alerts and warnings based on geographical location.
+    """
     def __init__(self,forecast_alert_data):
+        """
+        Initializes the WeatherAlert analyzer with weather data
+
+        :param forecast_alert_data: The raw weather forecast data response in JSON format
+        """
         self.forecast_alert_data = forecast_alert_data
 
     def filter_alert_metrics(self):
@@ -19,7 +27,6 @@ class Alert:
 
         :return: List containing the latest weather alert data
         """
-
         if (self.forecast_alert_data):
             last_weather_alert_index = self.forecast_alert_data[-1]
             return  {"headline":last_weather_alert_index ["headline"],"msgtype":last_weather_alert_index ["msgtype"],
@@ -32,15 +39,30 @@ class Alert:
             return None
 
     def alert_summary(self):
+        """
+        Generates a summary of any active weather alerts or warnings
+
+        :return: A formatted string describing the status of weather alerts for the forecast period
+        """
         string_builder = StringIO()
         time_period_forecast = self.filter_alert_metrics()
         string_builder.write(f"Alert: {self.alert_status(time_period_forecast)}\n")
         return string_builder.getvalue()
 
     def alert_status(self,time_period_forecast):
+        """
+        Assesses whether the array contains any alert data and returns the status as a string
+
+        :returns: 'ACTIVE' and its severity if an alert is detected, otherwise 'NOT ACTIVE'
+        """
         return f"🟩 NOT ACTIVE" if not (time_period_forecast) else f"🟩 ACTIVE({time_period_forecast["severity"].upper()})"
 
     def alert_report(self):
+        """
+        Generates a formatted summary of active weather alerts and warnings
+
+        :return: A structured string report containing headers, status, and detailed information
+        """
         time_period_forecast = self.filter_alert_metrics()
         string_builder = StringIO()
         alert_status= self.alert_status(time_period_forecast)
