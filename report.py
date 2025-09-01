e64ea3fb88bde12d8cdc92a699a1bbc357093d4f
alwinglee <alwin.g.lee@gmail.com>
2025-09-01 15:15:48 -0400

    Update the structure of the report_series dictionary

--- report.py
+++ report.py
@@ -5,17 +5,20 @@ class Report:
     """
     Gathers reports from other metric classes into the main Report class, preparing the data for full report generation
     """
-    def __init__(self,location_details, daylight_details, rain_details, wind_details, temperature_details,condition_details,START_TIME,END_TIME,total_hours):
+    def __init__(self,location_details, daylight_details, rain_details, wind_details, temperature_details,
+                 condition_details, date_details, alert_details, START_TIME,END_TIME):
         self.location_details= location_details
         self.daylight_details = daylight_details
         self.rain_details=rain_details
         self.wind_details = wind_details
         self.temperature_details = temperature_details
         self.condition_details=condition_details
+        self.date_details=date_details
+        self.alert_details=alert_details
         self.START_TIME = self.convert_to_datetime(START_TIME)
         self.END_TIME = self.convert_to_datetime(END_TIME)
-        self.total_hours = total_hours
         self.format_report()
+
 
     def convert_to_datetime(self,time):
         """
@@ -35,15 +38,40 @@ class Report:
 
         :return: None
         """
-        print (f"{self.location_details}\n"
+        report_series= {
+            "summary_report":
+            f"\n= = = 🗺️ LOCATION 🗺️ = = ="
+            f"\n{self.location_details}"
               f"{self.daylight_details}\n"
-              f"\n- - - SUMMARY - - -\n"
-              f"Coverage: {self.START_TIME} - {self.END_TIME} ({self.total_hours} hours)\n"
+              f"\n= = = 📝 SUMMARY 📝 = = =\n"
+              f"{self.date_details}\n"
               f"{self.condition_details.condition_summary()}"
+              f"{self.alert_details.alert_summary()}"
               f"{self.rain_details.rain_summary()}"
               f"{self.wind_details.wind_summary()}"
               f"{self.temperature_details.temperature_summary()}"
-              f"{self.rain_details.compile_pre_window_rain_report()}"
-              f"{self.rain_details.compile_during_window_rain_report()}"
-              f"{self.wind_details.compile_wind_report()}"
-              f"{self.temperature_details.compile_temperature_report()}")
+              f"{self.alert_details.alert_report()}"
+             f"{self.rain_details.compile_pre_window_rain_report()}"
+             f"{self.rain_details.compile_during_window_rain_report()}",
+            "wind_report":f"{self.wind_details.compile_wind_report()}",
+            "temperature_report":f"{self.temperature_details.compile_temperature_report()}",
+        }
+        return report_series
+
+        # print(f"{self.date_details.display_forecast_date()} Forecast | Message 1/3\n"
+        #         f"\n= = = 🗺️ LOCATION 🗺️ = = =\n"
+        #         f"{self.location_details}"
+        #         f"{self.daylight_details}\n"
+        #         f"\n= = = 📝 SUMMARY 📝 = = =\n"
+        #         f"{self.date_details}\n"
+        #         f"{self.condition_details.condition_summary()}"
+        #         f"{self.alert_details.alert_summary()}"
+        #         f"{self.rain_details.rain_summary()}"
+        #         f"{self.wind_details.wind_summary()}"
+        #         f"{self.temperature_details.temperature_summary()}"
+        #         f"{self.alert_details.alert_report()}"
+        #         f"{self.rain_details.compile_pre_window_rain_report()}"
+        #         f"{self.rain_details.compile_during_window_rain_report()}"
+        #         f"{self.wind_details.compile_wind_report()}"
+        #         f"{self.temperature_details.compile_temperature_report()}\n"
+        #         f"{self.date_details.display_report_date()}")
