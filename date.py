9a642c3c8d83262da3b79ebf627857d2570dac0e
alwinglee <alwin.g.lee@gmail.com>
2025-09-03 17:03:26 -0400

    Change START_TIME and END_TIME parameters to snake case

--- date.py
+++ date.py
@@ -3,19 +3,17 @@ class Date:
     """
     Represents a specific date and time for weather forecast data
     """
-    def __init__(self,START_TIME,END_TIME,forecast_data):
+    def __init__(self,start_time,end_time,forecast_data):
         """
         Initializes a Date object representing a specific hourly forecast timeframe
 
-        :param START_TIME: Start time of the hourly timeframe
-        :param END_TIME: End time of the hourly timeframe
+        :param start_time: Start time of the hourly timeframe
+        :param end_time: End time of the hourly timeframe
         :param forecast_data: Weather forecast data in JSON format
 
-        :return: None
         """
-        self.START_TIME = START_TIME
-        self.END_TIME = END_TIME
-        self.duration = END_TIME-START_TIME
+        self.start_time = start_time
+        self.end_time = end_time
         self.forecast_data = forecast_data
 
     def display_forecast_date(self):
@@ -28,18 +26,17 @@ class Date:
 
     def display_report_date(self):
         """
-        Creates a timestamp indicating when this weather forecast report was generated.
+        Creates a timestamp indicating when this weather forecast report was generated
 
         :return: Formatted string with the current date and time
         """
         return f"Generated {datetime.now().strftime("%Y-%m-%d | %H:%M %Z")}"
 
-    def __str__(self):
+    def display_timeframe(self):
         """
         Formats the start time, end time, and total duration for display
 
         :return: Formatted string showing the time range and duration
         """
-        return (f"Hours: {self.START_TIME}:00 - {self.END_TIME}:00 ({self.duration}h)")
-
+        return (f"Hours: {self.start_time}:00 - {self.end_time}:00 ({self.end_time-self.start_time}h)")
 
