0f9cdb7184b1481066bc13c5d3bf04e3473bbe6a
alwinglee <alwin.g.lee@gmail.com>
2025-09-16 12:13:59 -0400

    Update the variable name from 'time_period_forecast' to 'condition_data'

--- condition.py
+++ condition.py
@@ -1,34 +1,31 @@
 from statistics import mode
 class Condition:
     """
-    A class that processes weather data, evaluates the condition and generates a report.
+    A class that processes weather data, evaluates the condition and generates a report
     """
     def __init__(self,hourly_selected_forecast_data):
         self.hourly_selected_forecast_data = hourly_selected_forecast_data
 
     def filter_condition_metrics(self):
         """
-        Generates an hourly list within a specified time period and extracts weather condition texts and codes.
+        Generates an hourly list within a specified time period and extracts weather condition texts and codes
 
-        Conditions can include: Sunny, Mist, Overcast, etc.
+        Conditions can include: Sunny, Mist, Overcast, etc
 
-        :return: A time-sliced list containing hourly condition texts and codes.
+        :return: A time-sliced list containing hourly condition texts and codes
         """
-        timeline = [
-            {"condition_text": each_hour["condition"]["text"], "condition_code": each_hour["condition"]["code"]}
-            for each_hour in self.hourly_selected_forecast_data]
-        return timeline
+        return [{"condition_text": each_hour["condition"]["text"], "condition_code": each_hour["condition"]["code"]}
+                for each_hour in self.hourly_selected_forecast_data]
 
     def find_condition_mode(self):
         """
         Finds the most frequently appeared condition value
 
-        :return:  The most common condition value observed during the specified period
+        :return: The most common condition value observed during the specified period
         """
-        time_period_forecast = self.filter_condition_metrics()
-        condition_list= [each_hour["condition_text"] for each_hour in time_period_forecast]
-        dominant_condition= mode(condition_list)
-        return (dominant_condition)
+        condition_data = self.filter_condition_metrics()
+        condition_list = [each_hour["condition_text"] for each_hour in condition_data]
+        return mode(condition_list)
 
     def condition_summary(self):
         """
