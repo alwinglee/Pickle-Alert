65358c239e19b7a7066dd1bb4dad95f17c92e40b
alwinglee <alwin.g.lee@gmail.com>
2025-09-16 12:54:03 -0400

    Fix spacing in the code

--- weather_api.py
+++ weather_api.py
@@ -4,24 +4,24 @@ from dotenv import load_dotenv
 
 class Weather_API:
     """
-    A class to interact with a weather API and fetch forecast data.
+    A class to interact with a weather API and fetch forecast data
     """
     def fetch_weather_forecast(days_to_show):
         """
         Sends a request to the API server with specific parameters to retrieve the weather forecast for the current day
-        and the next two days.
+        and the next two days
 
         :param days_to_show: Number of days to include in forecast
 
-        :return: The API's weather forecast response in JSON format.
+        :return: The API's weather forecast response in JSON format
         """
         try:
             load_dotenv()
             weather_response = requests.get(os.getenv("URL"), params={"q": f"{os.getenv("LAT")},{os.getenv("LON")}",
                                                                       "key": os.getenv("API_KEY"), "days": days_to_show,
-                                                                      "alerts":"yes"})
+                                                                      "alerts": "yes"})
             weather_response.raise_for_status()
-            weather_response_json= weather_response.json()
+            weather_response_json = weather_response.json()
         except requests.exceptions.ConnectionError:
             raise Exception(f"NETWORK ERROR: FAILED TO CONNECT TO SERVER")
         except requests.exceptions.HTTPError:
