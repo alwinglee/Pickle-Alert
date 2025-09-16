4dfd8814718b5fc74105ec224976a992e20e13f7
alwinglee <alwin.g.lee@gmail.com>
2025-09-16 12:36:45 -0400

    Include a docstring for function 'retrieve_location_details'

--- location.py
+++ location.py
@@ -1,27 +1,26 @@
 from datetime import datetime
 class Location:
     """
-    Represents the geographical location (city, region, country) and local time for specific coordinates.
+    Represents the geographical location (city, region, country) and local time for specific coordinates
     """
     def __init__(self,forecast_location_data):
-        """
-        Initializes Location with geographic details and local time extracted from weather API response.
-
-        :param weather_data: Weather forecast response in JSON format.
-
-        :return: None
-        """
         self.forecast_location_data = forecast_location_data
 
     def retrieve_location_details(self, metric):
+        """
+        Retrieves the details of the geographical location
+
+        :param metric: Attributes of the geographical location such as 'name', 'region', and 'country'
+
+        :return: String value of the retrieved location attribute
+        """
         return self.forecast_location_data[metric]
 
     def __str__(self):
         """
-        Overrides the default '__str__' method to format location details and local time.
+        Overrides the default '__str__' method to format location details and local time
 
-        :return: A formatted string with city, region, and country comma-separated on one line,
-        and local time on a new line.
+        :return: A formatted string with city, region, and country
         """
         return (f"{self.retrieve_location_details("name")}, {self.retrieve_location_details("region")}, "
                 f"{self.retrieve_location_details("country")}")
