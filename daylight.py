64c51fa2d2a02e7c43d72105a6557b73b2adab98
alwinglee <alwin.g.lee@gmail.com>
2025-09-16 12:23:25 -0400

    Include a docstring for function 'retrieve_twilight_time'

--- daylight.py
+++ daylight.py
@@ -4,28 +4,27 @@ class Daylight:
     Represents sunrise and sunset times for a location
     """
     def __init__(self,forecast_data):
-        """
-        Initializes Daylight with sunrise/sunset times extracted from weather API response.
-
-        :param forecast_data: Filtered weather forecast response in JSON format.
-
-        :return: None
-        """
         self.forecast_data = forecast_data
 
     def convert_to_military_time(self,time):
         """
-        Converts standard time from API data to military time (24-hour format) for sunrise or sunset.
+        Converts standard time from API data to military time (24-hour format) for sunrise or sunset
 
-        :param time: Sunrise or sunset time in standard 12-hour format.
+        :param time: Sunrise or sunset time in standard 12-hour format
 
-        :return: Sunrise or sunset time in military (24-hour) format.
+        :return: Sunrise or sunset time in military (24-hour) format
         """
         return datetime.strptime(time,"%I:%M %p").strftime("%H:%M")
 
     def retrieve_twilight_time(self,metric):
-        return self.forecast_data["astro"][metric]
+        """
+        Retrieves the sunset or sunrise time
 
+        :param metric: Holds either key word 'sunrise' or 'sunset'
+
+        :return: String time of the sunset or sunrise time for the forecasted day
+        """
+        return self.forecast_data["astro"][metric]
 
     def __str__(self):
         """
