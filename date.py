0be5373176b4926176c4dd5819f2531627f53d23
alwinglee <alwin.g.lee@gmail.com>
2025-09-23 11:43:29 -0400

    Include the convert_to_datetime() function and refractor return statement for display_timeframe_summary()
    convert_to_datetime(): Takes user start and end time inputs and converts them into datetime objects for easier manipulation

--- date.py
+++ date.py
@@ -3,17 +3,9 @@ class Date:
     """
     Represents a specific date and time for weather forecast data
     """
-    def __init__(self,start_time,end_time,forecast_data):
-        """
-        Initializes a Date object representing a specific hourly forecast timeframe
-
-        :param start_time: Start time of the hourly timeframe
-        :param end_time: End time of the hourly timeframe
-        :param forecast_data: Weather forecast data in JSON format
-
-        """
-        self.start_time = start_time
-        self.end_time = end_time
+    def __init__(self, start_time, end_time, forecast_data):
+        self.start_time = self.convert_to_datetime(start_time)
+        self.end_time = self.convert_to_datetime(end_time)
         self.forecast_data = forecast_data
 
     def display_forecast_date(self):
@@ -22,21 +14,32 @@ class Date:
 
         :return: Formatted string representation of the forecast date
         """
-        return f"{self.forecast_data["date"]}"
+        return f"Forecast {self.forecast_data["date"]}"
 
-    def display_report_date(self):
+    def display_generation_date(self):
         """
         Creates a timestamp indicating when this weather forecast report was generated
 
         :return: Formatted string with the current date and time
         """
-        return f"Generated {datetime.now().strftime("%Y-%m-%d | %H:%M %Z")}"
+        return f"Generated {datetime.now().strftime("%Y-%m-%d | %H:%M%Z")}"
 
-    def display_timeframe(self):
+    def display_timeframe_summary(self):
         """
         Formats the start time, end time, and total duration for display
 
         :return: Formatted string showing the time range and duration
         """
-        return (f"Hours: {self.start_time}:00 - {self.end_time}:00 ({self.end_time-self.start_time}h)")
+        return f"Hours: {self.start_time}- {self.end_time}"
 
+    def convert_to_datetime(self,time):
+        """
+        Converts an integer time value (start_time or end_time) to a datetime object.
+
+        :param time: An integer representing either the start time or end time in hours (24-hour format)
+
+        :return: A datetime object formatted to display military time (hours and minutes)
+        """
+        if time == 24:
+            return datetime.strptime(f"23:59", "%H:%M").strftime("%H:%M")
+        return datetime.strptime(f"{time:02d}:00", "%H:%M").strftime("%H:%M")
