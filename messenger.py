645bff9402f7adfdca9dcf25ba4d3e34fd0d436a
alwinglee <alwin.g.lee@gmail.com>
2025-11-04 09:08:48 -0500

    Fix logic error in split_report function

--- messenger.py
+++ messenger.py
@@ -1,12 +1,15 @@
 import os
 from twilio.rest import Client
+
+
 class Messenger:
     """
     Connects to the Twilio API to send weather forecast reports to an authorized phone number
     """
     # The limit is 1024; however, it was reduced account for forecast and generation date details
     TWILIO_WHATSAPP_CHARACTER_LIMIT = 950
-    def __init__(self, report_details,date_details):
+
+    def __init__(self, report_details, date_details):
         self.report_content = report_details.formatted_report
         self.date_details = date_details
         self.send_message()
@@ -20,12 +23,17 @@ class Messenger:
         """
         report_segments = []
         content = self.report_content
-        if len(content) < self.TWILIO_WHATSAPP_CHARACTER_LIMIT:
-            return report_segments.append(content)
+
+        if not content:
+            return report_segments
+        if len(content) <= self.TWILIO_WHATSAPP_CHARACTER_LIMIT:
+            report_segments.append(content)
+            return report_segments
+
         while content and len(content) > 0:
             current_segment = content[:self.TWILIO_WHATSAPP_CHARACTER_LIMIT]
             new_line_index = current_segment.rfind("\n")
-            if (new_line_index ==-1):
+            if new_line_index == -1:
                 report_segments.append(current_segment)
                 content = content[self.TWILIO_WHATSAPP_CHARACTER_LIMIT:]
             else:
@@ -37,7 +45,8 @@ class Messenger:
         """
         Sends the weather forecast report as a message using Twilio
 
-        :return: A formatted string detailing the forecasted day's weather, including its generation date and report count
+        :return: A formatted string detailing the forecasted day's weather, including its generation date and
+         report count
         """
         account_sid = os.getenv("TWILIO_ACCOUNT_SID")
         auth_token = os.getenv("TWILIO_AUTH_TOKEN")
