4547c7623fda25513b5df5609f8f51e70b9b5d0c
alwinglee <alwin.g.lee@gmail.com>
2025-11-04 10:17:49 -0500

    Include new instance in activate_validation function

--- configuration.py
+++ configuration.py
@@ -1,23 +1,27 @@
 from configuration_validator import Configuration_Validator
+
+
 class Configuration:
     """
     Stores weather configuration parameters and passes them to the Configuration_Validator class for validation
     """
     def __init__(self, start_time, end_time, top_timeline_count, rain_check_hours_prior, days_to_show):
-        self.start_time=start_time
-        self.end_time=end_time
-        self.top_timeline_count=top_timeline_count
-        self.rain_check_hours_prior=rain_check_hours_prior
-        self.days_to_show=days_to_show
+        self.start_time = start_time
+        self.end_time = end_time
+        self.top_timeline_count = top_timeline_count
+        self.rain_check_hours_prior = rain_check_hours_prior
+        self.days_to_show = days_to_show
         self.duration = self.end_time - self.start_time
-        self.activateValidation()
-    def activateValidation(self):
+        self.activate_validation()
+
+    def activate_validation(self):
         """
         Executes all configuration validations functions
 
-        :exception: An error message will be displayed when validation conditions are not met
+        :exception: An Exception will be displayed when validation conditions are not met
         """
-        Configuration_Validator.validate_forecast_days(self.days_to_show)
-        Configuration_Validator.validate_time_range(self.start_time,self.end_time)
-        Configuration_Validator.validate_timeline_fits_range(self.duration,self.top_timeline_count)
-        Configuration_Validator.validate_rain_check_window(self.rain_check_hours_prior,self.start_time)
+        validator = Configuration_Validator()
+        validator.validate_forecast_days(self.days_to_show)
+        validator.validate_time_range(self.start_time, self.end_time)
+        validator.validate_timeline_fits_range(self.duration, self.top_timeline_count)
+        validator.validate_rain_check_window(self.rain_check_hours_prior, self.start_time)
