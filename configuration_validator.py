5ef9f47fdc532388c247d5e9d45b84e9f3d7335b
alwinglee <alwin.g.lee@gmail.com>
2025-11-04 10:20:28 -0500

    Remove @classmethod to reflect changes in configuration.py

--- configuration_validator.py
+++ configuration_validator.py
@@ -1,10 +1,9 @@
-
 class Configuration_Validator:
     """
     Validates weather configuration parameters
     """
-    @classmethod
-    def validate_forecast_days(self,days_to_show):
+
+    def validate_forecast_days(self, days_to_show):
         """
         Validates the parameter days_to_show to ensure it is within the minimum (1 day) and maximum (14 days) range
         Note: The WeatherAPI free account only allows up to a maximum of three forecast days to be displayed
@@ -16,8 +15,7 @@ s
         if not (1 <= days_to_show <= 14):
             raise Exception(f"- CAN ONLY FORECAST BETWEEN 1 AND 14 DAYS")
 
-    @classmethod
-    def validate_time_range(self,start_time,end_time):
+    def validate_time_range(self, start_time, end_time):
         """
         Validates that the start_time and end_time are within the allowed range and ensures that end_time is greater
         than start_time
@@ -29,12 +27,10 @@ s
         """
         if not (0 <= start_time <= 23) or not (1 <= end_time <= 24):
             raise Exception("- INVALID TIME. ENTER THE HOUR IN 24-HOUR FORMAT (0-24) AS A WHOLE NUMBER")
-        if end_time <=start_time:
+        if end_time <= start_time:
             raise Exception(f"- END TIME MUST BE AFTER START TIME")
 
-
-    @classmethod
-    def validate_timeline_fits_range(self,duration, top_timeline_count):
+    def validate_timeline_fits_range(self, duration, top_timeline_count):
         """
         Ensures that the number of hours displayed in the timeline report does not exceed the total duration of the
         analysis period
@@ -44,11 +40,10 @@ s
 
         :exception: An error message if the condition is not met
         """
-        if (duration < top_timeline_count):
+        if duration < top_timeline_count:
             raise Exception(f"- TIMELINE COUNT TOO LONG (MAX {duration} HOURS)")
 
-    @classmethod
-    def validate_rain_check_window(self,rain_check_hours_prior,start_time):
+    def validate_rain_check_window(self, rain_check_hours_prior, start_time):
         """
         Validates that the rain check window occurs on the same day and before the analysis start time
 
@@ -57,6 +52,5 @@ s
 
         :exception: An error message if the condition is not met.
         """
-        if (rain_check_hours_prior>start_time or start_time - rain_check_hours_prior<0):
+        if rain_check_hours_prior > start_time or start_time - rain_check_hours_prior < 0:
             raise Exception(f"- RAIN CHECK PERIOD MUST BE SAME DAY AND BEFORE START TIME")
-
